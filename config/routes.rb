756b8252b47b6c6b47d89c986863688241337ffe
rafaelbsavi <73194818+rafaelbsavi@users.noreply.github.com>
2024-05-07 23:35:39 +0000

    update venue working

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,17 @@ Rails.application.routes.draw do
   get("/users", { :controller => "users", :action => "index" })
   get("/users/:username", { :controller => "users", :action => "show" })
   post("/update_user/:user_id", { :controller => "users", :action => "update" })
-  
   get("/insert_user_record", { :controller => "users", :action => "create" })
   
 
   get("/venues", { :controller => "venues", :action => "index" })
-  post("/insert_venue_record", { :controller => "venues", :action => "create" })
-  get("/venue/:an_id", { :controller => "venue", :action => "show" })
-  post("/update_venue/:the_id", { :controller => "application", :action => "update" })
-  get("/delete_venue/:id_to_delete", { :controller => "venue", :action => "destroy" })
+  get("/insert_venue_record", { :controller => "venues", :action => "create" })
+  
+  get("/venues/:an_id", { :controller => "venues", :action => "show" })
+  
+  post("/update_venue/:the_id", { :controller => "venues", :action => "update" })
+
+  get("/delete_venue/:id_to_delete", { :controller => "venues", :action => "destroy" })
   
   post("/insert_comment_record", { :controller => "comments", :action => "create" })
 end
